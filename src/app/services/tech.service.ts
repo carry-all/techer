7d96e7f0fa16e0cd40d805b4db3e630a294e5071
Your Name <you@example.com>
2018-01-30 23:38:06 +0300

    Implemented auto list update

--- src/app/services/tech.service.ts
+++ src/app/services/tech.service.ts
@@ -1,10 +1,11 @@
 import {Injectable} from "@angular/core";
-import {Http/*, Response*/} from "@angular/http";
+import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
 import {Tech} from "../tech";
 import "rxjs/add/operator/toPromise";
+import {Subject} from "rxjs/Subject";
 
 /*
 const techs: Tech[] = [
@@ -15,27 +16,40 @@ const techs: Tech[] = [
 
 @Injectable()
 export class TechService {
-    url = "http://192.168.0.103:8080";
+  url = "http://192.168.0.103:8080";
+  private subject = new Subject<Tech[]>();
 
-    constructor(private http: Http) {
-    }
+  constructor(private http: Http) {
+  }
 
-    techList(): Observable<Tech[]> {
-      return this.http.get(this.url + "/techs")
-        .map(response => response.json() as Tech[])
-        .catch(this.handleError); // todo: seems strange, what should I return instead of a Promise?
-      // todo: need to store observeable here + put code to clean on unsubscribe
-    }
+  techList(): Observable<Tech[]> {
+    this.updateList();
 
-    addTech(tech: Tech): Promise<Tech> {
-        return this.http.put(this.url + "/tech?title=" + tech.title, tech)
-            .toPromise()
-            .then(response => response.json() as Tech)
-            .catch(this.handleError);
-    }
+    // return subject as is. It would be updated in any case
+    return this.subject.asObservable();
+  }
 
-    private handleError(error: any): Promise<any> {
-        console.error("Error occured: " + error);
-        return Promise.reject(error.message || error);
-    }
+  private updateList() {
+    console.log("updateList invoked")
+    // fire http get to update subject
+    this.http.get(this.url + "/techs")
+      .toPromise()
+      .then(response => this.subject.next(response.json() as Tech[])) // todo: promise won't be cancelled. What to do?
+      .catch(this.handleError);
+  }
+
+  addTech(tech: Tech): Promise<Tech> {
+    return this.http.put(this.url + "/tech?title=" + tech.title, tech)
+      .toPromise()
+      .then((response) => {
+        this.updateList();
+        return response.json() as Tech;
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error("Error occured: " + error);
+    return Promise.reject(error.message || error);
+  }
 }
